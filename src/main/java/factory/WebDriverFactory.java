8505f2afc2536910829f96121f99e1422eb029aa
nikolaj <skrabets@gmail.com>
2024-12-06 16:40:04 +0100

    homework_4

--- src/main/java/factory/WebDriverFactory.java
+++ src/main/java/factory/WebDriverFactory.java
@@ -6,9 +6,14 @@ import listeners.CommonListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WebDriverFactory {
 
@@ -16,15 +21,38 @@ public class WebDriverFactory {
 
     public WebDriver create() throws MalformedURLException {
         WebDriver driver;
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "Nexus 5");
 
-        //if (!System.getProperty("remote.url").isEmpty()) {
-        //    ChromeOptions options = new ChromeOptions();
-        //    options.setCapability("browserVersion", "128.0");
-        //    options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-        //        put("name", "Test badge");
-        //   }});
-        //    return new RemoteWebDriver(new URL(System.getProperty("remote.url")), options);
-        //}
+
+
+        if (!System.getProperty("remote.url").isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--disable-background-timer-throttling");
+            options.addArguments("--disable-backgrounding-occluded-windows");
+            options.addArguments("--disable-renderer-backgrounding");
+            options.setExperimentalOption("mobileEmulation", mobileEmulation);
+            options.setCapability("browserVersion", "128.0");
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+
+                put("enableVNC", true);
+                /* How to add test badge */
+                put("name", "Test badge...");
+
+                /* How to set session timeout */
+                put("sessionTimeout", "15m");
+
+                /* How to set timezone */
+                put("env", new ArrayList<String>() {{
+                    add("TZ=UTC");
+                }});
+
+                /* How to enable video recording */
+                put("enableVideo", false);
+            }});
+            return new RemoteWebDriver(new URL(System.getProperty("remote.url")), options);
+        }
 
         switch (browserName.trim().toLowerCase()) {
             case "chrome":
