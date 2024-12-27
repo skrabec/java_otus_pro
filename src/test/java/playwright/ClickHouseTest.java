596a280c976ad323ff79be75bc6063a67718c9c8
Nikolaj Skrabec <skrabets@gmail.com>
2024-12-27 21:16:31 +0100

    ClickHouse tests finished

--- src/test/java/playwright/ClickHouseTest.java
+++ src/test/java/playwright/ClickHouseTest.java
@@ -12,12 +12,19 @@ public class ClickHouseTest {
     @Inject
     private ClickHousePage clickHousePage;
 
+    String teacherName = "Алексей Железной";
+
     @Test
     public void clickHouseTest() {
         clickHousePage.open();
-        //clickHousePage.scrollInstructorsListByDragAndDrop();
-        clickHousePage.clickOnTheSlideByName("Алексей Железной");
-        clickHousePage.checkTeacherNamePresent("Алексей Железной");
-        clickHousePage.clickOnArrow();
+        clickHousePage.scrollInstructorsListByDragAndDrop(400);
+        clickHousePage.checkIfInstructorSlidesWereRotated();
+        clickHousePage.scrollInstructorsListByDragAndDrop(-400);
+        clickHousePage.clickOnTheSlideByName(teacherName);
+        clickHousePage.checkTeacherNamePresent(teacherName);
+        clickHousePage.clickOnArrowRight();
+        clickHousePage.validateDifferentTeacherName(teacherName);
+        clickHousePage.clickOnArrowLeft();
+        clickHousePage.checkTeacherNamePresent(teacherName);
     }
 }
